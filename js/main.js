865fa8c5ce3bcae5f8924745261df1722cc323bd
Jesse <jessedrebin@gmail.com>
2018-04-06 11:02:06 -0700

    add html for program

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-
+// Business Logic
 function Pizza (toppings, size) {
 this.toppings = topppings,
 this.size = size
 }
+
+
+
+
+
+// User Interface logic
+// $(document).ready(function() {
+//
+//
+// };
